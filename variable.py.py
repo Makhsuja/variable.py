c8881c0ed9b9ca360116682dbc1ded08a58394af
Makhsuja <makhsuja18@gmail.com>
2024-10-15 17:42:33 +0300

    Add files via upload

---
+++ variable.py.py
@@ -0,0 +1,5 @@
+Number_of_completed_DPS  = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_per_task = 0.125
+print("Курс: 'Python'" , "всего задач: 12" , "затрачено часов: 1.5" , "среднее время выполнения: 0.125")
